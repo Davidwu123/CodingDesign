2cd575f42f75f09713f23a82f6fbd057edfa2c24
wei.wu@aispeech.com <wuwei123>
2020-02-26 12:02:31 +0800

    add builder pattern

---
+++ src/com/study/design/pattern/builder/ResourcePoolConfig.java
@@ -0,0 +1,113 @@
+package com.study.design.pattern.builder;
+
+/**
+ * @author wuwei
+ * @title: ResourcePoolConfig
+ * @projectName CodingDesign
+ * @description: TODO
+ * @date 2020-02-26 10:30
+ */
+public class ResourcePoolConfig {
+    private String name;
+    private int minIdle;
+    private int maxIdle;
+    private int maxTotal;
+
+    private ResourcePoolConfig(Builder builder) {
+        this.name = builder.name;
+        this.minIdle = builder.minIdle;
+        this.maxIdle = builder.maxIdle;
+        this.maxTotal = builder.maxTotal;
+    }
+
+    @Override
+    public String toString() {
+        return "ResourcePoolConfig{" +
+                "name='" + name + '\'' +
+                ", minIdle=" + minIdle +
+                ", maxIdle=" + maxIdle +
+                ", maxTotal=" + maxTotal +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMinIdle() {
+        return minIdle;
+    }
+
+    public int getMaxIdle() {
+        return maxIdle;
+    }
+
+    public int getMaxTotal() {
+        return maxTotal;
+    }
+
+    public static class Builder {
+        private static final int DEFAULT_MIN_IDLE = 2;
+        private static final int DEFAULT_MAX_IDLE = 5;
+        private static final int DEFAULT_MAX_TOTAL = 8;
+        private String name;
+        private int minIdle = DEFAULT_MIN_IDLE;
+        private int maxIdle = DEFAULT_MAX_IDLE;
+        private int maxTotal = DEFAULT_MAX_TOTAL;
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        /**
+         * 可选设置
+         * @param minIdle
+         * @return
+         */
+        public Builder setMinIdle(int minIdle) {
+            this.minIdle = minIdle;
+            return this;
+        }
+
+        /**
+         * 可选设置
+         * @param maxIdle
+         * @return
+         */
+        public Builder setMaxIdle(int maxIdle) {
+            this.maxIdle = maxIdle;
+            return this;
+        }
+
+        /**
+         * 可选设置
+         * @param maxTotal
+         * @return
+         */
+        public Builder setMaxTotal(int maxTotal) {
+            this.maxTotal = maxTotal;
+            return this;
+        }
+
+        public ResourcePoolConfig build() {
+            //这里可以有可以参数合法性，依赖性等业务性检查
+            if (name == null || name.equals("")) {
+                throw new IllegalArgumentException("invalid name");
+            }
+            if (minIdle < 0) {
+                throw new IllegalArgumentException("invalid minIdle");
+            }
+            if (maxIdle < 0) {
+                throw new IllegalArgumentException("invalid maxIdle");
+            }
+            if (maxTotal < 0) {
+                throw new IllegalArgumentException("invalid maxTotal");
+            }
+            if (maxTotal < minIdle || maxTotal < maxIdle) {
+                throw new IllegalArgumentException("invalid maxTotal");
+            }
+            return new ResourcePoolConfig(this);
+        }
+    }
+}
--- src/com/study/design/pattern/factory/player/TestMain.java
+++ src/com/study/design/pattern/factory/player/TestMain.java
@@ -4,16 +4,15 @@ import com.study.design.common.Log;
 import com.study.design.pattern.factory.player.method.IPlayerFactory;
 import com.study.design.pattern.factory.player.method.PlayerFactoryMap;
 import com.study.design.pattern.factory.player.simple.SimplePlayerFactory;
-import com.study.design.pattern.singleton.TestMain;
 
 /**
  * @author wuwei
- * @title: testMain
+ * @title: TestMain
  * @projectName CodingDesign
  * @description: TODO
  * @date 2020-02-24 09:29
  */
-public class testMain {
+public class TestMain {
 
     public static void main(String[] args) {
         executeSimpleFactory();
@@ -24,8 +23,8 @@ public class testMain {
         IPlayer player = SimplePlayerFactory.createByClazz(MediaPlayer.class);
 //        Log.d(TestMain.class, "simpleFactory player instance1 is " + player.toString());
 //        player = SimplePlayerFactoryV3.create("exo");
-        Log.d(TestMain.class, "simpleFactory player instance2 is " + player.toString());
-        Log.d(testMain.class, "simpleFactory do play");
+        Log.d(com.study.design.pattern.singleton.TestMain.class, "simpleFactory player instance2 is " + player.toString());
+        Log.d(TestMain.class, "simpleFactory do play");
         player.play();
     }
 
@@ -33,10 +32,10 @@ public class testMain {
         //易用性较差,需要再次封装工厂类，工厂类更高层的封装类
         IPlayerFactory playerFactory = PlayerFactoryMap.getFactory("media");
         IPlayer player = playerFactory.create();
-        Log.d(TestMain.class, "FactoryMethod instance1 is " + player.toString());
+        Log.d(com.study.design.pattern.singleton.TestMain.class, "FactoryMethod instance1 is " + player.toString());
         player = playerFactory.create();
-        Log.d(TestMain.class, "FactoryMethod instance2 is " + player.toString());
-        Log.d(testMain.class, "FactoryMethod do play");
+        Log.d(com.study.design.pattern.singleton.TestMain.class, "FactoryMethod instance2 is " + player.toString());
+        Log.d(TestMain.class, "FactoryMethod do play");
         player.play();
     }
 
